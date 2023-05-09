2412f013cef53d868e3971c652c7f5b61a692f6a
Rohitadesh <adeshnagalikar2000@gmail.com>
2023-05-09 18:07:47 +0530

    newly commit

--- tip.js
+++ tip.js
@@ -1,3 +1,17 @@
-let bill_amount=document.querySelector('.bill-amount');
-let tip_percent=document.querySelector('.tip');
-let total_person=document.querySelector('.total-person');
+
+    let amountBillElement = document.querySelector('.bill-amount');
+    let percentageTipElement =document.querySelector('.tip');
+    let peopleNumberElement = document.querySelector('.total-person');
+    let calculate=document.querySelector('.button')
+    let person_amount=document.querySelector('.each-person')
+    let bill_amount=document.querySelector('.result-amount')
+    let tip,total_amount,each_person;
+    calculate.addEventListener('click',()=>{
+        tip=(amountBillElement.value*percentageTipElement.value)/100; 
+        total_amount=parseInt(amountBillElement.value)+parseInt(tip)
+        each_person=(parseInt(tip*100)/parseInt(peopleNumberElement.value))/100
+        bill_amount.textContent=total_amount
+        person_amount.textContent=each_person
+
+    });
+   
